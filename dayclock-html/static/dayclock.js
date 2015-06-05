4cac940af864e559fd2d505e4ed854a1d379c11f
jbyrnes <jbyrnes@adeptusproductions.com>
2015-06-05 18:44:20 +1000

    adding javascript experiments' '

---
+++ dayclock-html/static/dayclock.js
@@ -0,0 +1,20 @@
+
+function strokeArc(stage, colour, style, x, y, radius, startRads, endRads, rotateRate) {
+    var arc = new createjs.Shape();
+
+    arc.graphics
+        .beginStroke(colour)
+        .setStrokeStyle(style)
+        .arc(0, 0, radius, startRads, endRads)
+        .setStrokeStyle(0)
+        .closePath();
+
+    // the shape is created with a center at 0,0 then moved to its x,y so rotation will work
+    arc.x = x;
+    arc.y = y;
+    arc.rotateRate = rotateRate;
+
+    //stage.addChild(arc);
+    return arc;
+}
+
