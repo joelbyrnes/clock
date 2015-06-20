4e313c511002858efc8d3295a945e0361ecb86ec
Joel Byrnes <fatal.exception.occurred@gmail.com>
2015-06-20 20:10:16 +1000

    added outer ring and hour breaks, made layers have local min and max radius

--- dayclock-html/static/dayclock.js
+++ dayclock-html/static/dayclock.js
@@ -15,7 +15,6 @@ CircularGraphics.prototype.sectorShape = function(cell) {
     arc.graphics
         .beginStroke(cell.color)
         .setStrokeStyle(cell.height)
-//        .arc(0, 0, cell.radius, cell.start * 2 * Math.PI, cell.end * 2 * Math.PI)
         .arc(0, 0, cell.radius + (cell.height / 2), cell.start * 2 * Math.PI, cell.end * 2 * Math.PI)
         .setStrokeStyle(0)
         .closePath();
@@ -23,6 +22,31 @@ CircularGraphics.prototype.sectorShape = function(cell) {
     // the shape is created with a center at 0,0 then moved to its x,y so rotation will work
     arc.x = this.xCenter;
     arc.y = this.yCenter;
+
+    arc.name = cell.name;
+    arc.rotateRate = cell.rotateRate;
+    arc.alpha = cell.alpha || 1.0;
+
+    return arc;
+};
+
+// arc from center
+CircularGraphics.prototype.arcShape = function(cell) {
+    var arc = new createjs.Shape();
+
+//    arc.graphics.arc(xCenter, yCenter, 20, 0 * Math.PI, 2.2 * Math.PI, true).lineTo(xCenter, yCenter).closePath();
+
+    arc.graphics
+        .beginFill(cell.color)
+        .setStrokeStyle(0)
+//        .moveTo(this.xCenter, this.yCenter)
+        .arc(this.xCenter, this.yCenter, cell.radius, cell.start * 2 * Math.PI, cell.end * 2 * Math.PI, false)
+        .lineTo(this.xCenter, this.yCenter)
+        .closePath();
+
+    // the shape is created with a center at 0,0 then moved to its x,y so rotation will work
+//    arc.x = this.xCenter;
+//    arc.y = this.yCenter;
 
     arc.name = cell.name;
     arc.rotateRate = cell.rotateRate;
@@ -78,15 +102,18 @@ var LayeredCircle = function (x, y, maxRadius) {
 LayeredCircle.prototype = Object.create(CircularGraphics.prototype);
 LayeredCircle.prototype.constructor = LayeredCircle;
 
-LayeredCircle.prototype.addLayers = function(rows) {
+LayeredCircle.prototype.addLayers = function(rows, maxRadius, minRadius) {
+    maxRadius = maxRadius || this.maxRadius;
+    minRadius = minRadius || this.minRadius;
+
     console.log('drawing ' + rows.length + ' layers');
-    var height = (this.maxRadius - this.minRadius - ((rows.length) * this.gap)) / (rows.length);
+    var height = (maxRadius - minRadius - ((rows.length) * this.gap)) / (rows.length);
 
     var cells = [];
 
     for (var r=0; r < rows.length; r++) {
         var row = rows[r];
-        var rad = (this.maxRadius - ((height + this.gap) * (r + 1)));
+        var rad = (maxRadius - ((height + this.gap) * (r + 1)));
 
         for (var c=0; c < row.length; c++) {
             var cell = row[c];
@@ -118,7 +145,8 @@ var Clock = function (x, y, maxRadius) {
     this.minRadius = maxRadius / 4;
     this.midnight = time(0,0);
     this.millis24hour = 86400000; // 24 * 60 * 60 * 1000;
-    this.centerColor = "#000000";
+    this.bgColor = "#000000";
+    this.centerColor = this.bgColor;
     this.activities = [];
 };
 
@@ -133,6 +161,10 @@ Clock.prototype.setActivities = function(activities) {
 Clock.prototype.drawTimePeriods = function(activities) {
     // for now every activity has its own layer - later, merge ones that don't overlap.
 
+    // outer ring
+    console.log("add ring");
+    this.addSector({name: "__outer", radius:this.maxRadius-this.gap*3, height: this.gap * 2, start: 0, end: 1, color: "rgb(0, 255, 0)", alpha: 210});
+
     var layers = [];
 
     for (var i=0; i < activities.length; i++) {
@@ -142,10 +174,7 @@ Clock.prototype.drawTimePeriods = function(activities) {
         layers.push([sector]);
     }
 
-    // outer ring
-//            this.addChild(sectorShape(this.xCenter, this.yCenter, {name: "outer", radius:this.maxRadius-2, height: 2, start: 0, end: 1, color: "#505090"}));
-
-    this.addLayers(layers);
+    this.addLayers(layers, this.maxRadius-this.gap*3);
 };
 
 // effectively turns times into start and end positions
@@ -153,12 +182,27 @@ Clock.prototype.calculateSector = function(act) {
     // lazy copy - TODO fix
     var period = JSON.parse(JSON.stringify(act));
 
-    // adjust the rendering so midnight is at the top
-    // TODO what is the 0.25??
+    // adjust the rendering so midnight is at the top - rotate back by a quarter
     period.start = ((act.start - this.midnight) / this.millis24hour) - 0.25;
     period.end = ((act.end - this.midnight) / this.millis24hour) - 0.25;
 
     return period;
+};
+
+Clock.prototype.drawSegmentBreaks = function() {
+    // break concentric circles into 24 segments
+    for (var h = 0; h < 24; h++) {
+//        console.log("draw hour marker " + h);
+        this.drawSpoke(h / 24.0, this.bgColor);
+    }
+
+    // TODO make arcs thinner or use straight lines. minimum appears to be 0.0036.
+
+//    this.addChild(this.arcShape({radius: this.maxRadius, start: 0, end: 0.0036, color: "red", xalpha: 0.5}));
+};
+
+Clock.prototype.drawSpoke = function(position, color) {
+    this.addChild(this.arcShape({radius: this.maxRadius, start: position - 0.0018, end: position + 0.0018, color: color, xalpha: 0.5}));
 };
 
 Clock.prototype.drawTimePassedShadow = function() {
@@ -182,13 +226,13 @@ Clock.prototype.drawTimeAndDate = function() {
     var time = new createjs.Text(moment().format('H:mm'), timeSize + "px Arial", "#ffffff");
     time.textAlign = "center";
     time.x = this.xCenter;
-    time.y = this.yCenter - timeSize - this.gap;
+    time.y = this.yCenter - timeSize;
     this.addChild(time);
 
     var date = new createjs.Text(moment().format('ddd, MMM Do'), timeSize/2 + "px Arial", "#ffffff");
     date.textAlign = "center";
     date.x = this.xCenter;
-    date.y = this.yCenter;
+    date.y = this.yCenter + this.gap;
     this.addChild(date);
 };
 
@@ -196,7 +240,7 @@ Clock.prototype.update = function() {
     console.log("clock updating");
     this.removeAllChildren();
     this.drawTimePeriods(this.activities);
-    this.removeChild(this.getChildByName("__shadow"));
+    this.drawSegmentBreaks();
     this.drawTimePassedShadow();
     this.drawTimeAndDate();
 };
