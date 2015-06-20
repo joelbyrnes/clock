9ab14275484c79807bcdc335833fda0200aebe7d
Joel Byrnes <fatal.exception.occurred@gmail.com>
2015-06-20 16:08:13 +1000

    move data out of html

---
+++ dayclock-html/static/clockdata.js
@@ -0,0 +1,10 @@
+
+var activities = [
+    {name: "work",  start: time(9, 30),  end: time(18, 0),  color: "orange"},
+    {name: "sleep", start: time(0, 0),   end: time(7, 30),  color: "blue"},
+    {name: "foo",   start: time(18, 30), end: time(21, 30), color: "green"},
+    {},
+    {},
+    {}
+];
+
